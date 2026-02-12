aad0b5c58dad8d8a8d2fe2da4058292a3b399b32
monkescripts <shaolianghe0.0@gmail.com>
2026-02-12 22:14:14 +0800

    Update README

---
+++ include/dummy_secrets.h
@@ -0,0 +1,11 @@
+#ifndef ROOT_CA_H
+#define ROOT_CA_H
+#define WIFI_SSID "NUSSTU"
+#define WIFI_PASS "horhor"
+
+static const char my_root_ca[] =
+    "MIIB+zjeffjeffjeffwMwIDEeMBwGA1UEAxMV"
+    "blahblahblahbarabarabara"
+    "REST OF YOUR CERT"
+    "timtimtim==";
+#endif
